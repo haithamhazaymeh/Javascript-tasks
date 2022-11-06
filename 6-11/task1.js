9be06b4d1e5be3390136497ebb542f6bd8a2b75d
haithamhazaymeh <haithamhazaymeh@users.noreply.github.com>
2022-11-06 18:34:25 +0300

    6/11

---
+++ 6-11/task1.js
@@ -0,0 +1,39 @@
+let month = prompt ('enter of your month birth') );
+
+switch (month) {
+    case 1 :
+         document.write('january');
+          break;
+    case 2 :
+         document.write ('February ');
+          break;
+    case 3 :
+         document.write ('March ');
+          break;
+    case 4 :
+         document.write ('April ');
+          break;
+    case 5 :
+         document.write ('May ');
+          break;
+    case 6 :
+         document.write ('June ');
+          break;
+    case 7 :
+         document.write ('July ');
+          break;
+    case 8 :
+         document.write ('August ');
+          break;
+    case 9 :
+         document.write ('September ');
+          break;
+    case 10 :
+         document.write ('October ');
+          break;
+    case 11 :
+         document.write ('November ');
+          break;
+    case 12 : 
+    document.write ('December ');
+}
---
+++ 6-11/task3.js
@@ -0,0 +1,8 @@
+for (let x=0 ; x<=1000 ; x++) {
+
+
+    if (x%13==0) {
+        document.write(x+"\n")
+    }
+
+}
